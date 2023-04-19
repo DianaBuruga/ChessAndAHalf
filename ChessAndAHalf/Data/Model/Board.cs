2dec5ea3d9abb4e4fb076139aafcbca0b1b94e2f
DianaBuruga <100471189+DianaBuruga@users.noreply.github.com>
2023-04-19 13:59:52 +0300

    Add pieces on board

--- ChessAndAHalf/Data/Model/Board.cs
+++ ChessAndAHalf/Data/Model/Board.cs
@@ -28,12 +28,64 @@ namespace ChessAndAHalf.Data.Model
                 }
             }
 
-            squares[4, 4].Occupant = new Cat(PlayerColor.WHITE);
-            squares[0, 1].Occupant = new StarCat(PlayerColor.WHITE);
-            squares[8, 8].Occupant = new Cat(PlayerColor.BLACK);
-            squares[0, 3].Occupant = new StarCat(PlayerColor.BLACK);
+            /* squares[4, 4].Occupant = new Cat(PlayerColor.WHITE);
+             squares[0, 1].Occupant = new StarCat(PlayerColor.WHITE);
+             squares[8, 8].Occupant = new Cat(PlayerColor.BLACK);
+             squares[0, 3].Occupant = new StarCat(PlayerColor.BLACK);*/
+            StartPlacement();
         }
 
+        public void StartPlacement()
+        {
+            int column = 0, row =0;
+            squares[row, column].Occupant = new Rook(PlayerColor.BLACK);
+            squares[row, 11-column].Occupant = new Rook(PlayerColor.BLACK);
+            squares[11,column].Occupant = new Rook(PlayerColor.WHITE);
+            squares[11, 11-column].Occupant = new Rook(PlayerColor.WHITE);
+            column++;
+            squares[row, column].Occupant = new Knight(PlayerColor.BLACK);
+            squares[row, 11 - column].Occupant = new Knight(PlayerColor.BLACK);
+            squares[11, column].Occupant = new Knight(PlayerColor.WHITE);
+            squares[11, 11 - column].Occupant = new Knight(PlayerColor.WHITE);
+            column++;
+            squares[row, column].Occupant = new Bishop(PlayerColor.BLACK);
+            squares[row, 11 - column].Occupant = new Bishop(PlayerColor.BLACK);
+            squares[11, column].Occupant = new Bishop(PlayerColor.WHITE);
+            squares[11, 11 - column].Occupant = new Bishop(PlayerColor.WHITE);
+            column++;
+            squares[row, column].Occupant = new StarCat(PlayerColor.BLACK);
+            squares[row, 11 - column].Occupant = new StarCat(PlayerColor.BLACK);
+            squares[11, column].Occupant = new StarCat(PlayerColor.WHITE);
+            squares[11, 11 - column].Occupant = new StarCat(PlayerColor.WHITE);
+            column++;
+            squares[row, column].Occupant = new Cat(PlayerColor.BLACK);
+            squares[row, 11 - column].Occupant = new Cat(PlayerColor.BLACK);
+            squares[11, column].Occupant = new Cat(PlayerColor.WHITE);
+            squares[11, 11 - column].Occupant = new Cat(PlayerColor.WHITE);
+            column++;
+            squares[row, column].Occupant = new Queen(PlayerColor.BLACK);
+            squares[row, 11 - column].Occupant = new King(PlayerColor.BLACK);
+            squares[11, column].Occupant = new Queen(PlayerColor.WHITE);
+            squares[11, 11 - column].Occupant = new King(PlayerColor.WHITE);
+            row++;
+            for(column = 0; column < 6; column++)
+            {
+                if (column == 3)
+                {
+                    squares[row, column].Occupant = new Guard(PlayerColor.BLACK);
+                    squares[row, 11 - column].Occupant = new Guard(PlayerColor.BLACK);
+                    squares[11-row, column].Occupant = new Guard(PlayerColor.WHITE);
+                    squares[11-row, 11 - column].Occupant = new Guard(PlayerColor.WHITE);
+                }
+                else
+                {
+                    squares[row, column].Occupant = new Pawn(PlayerColor.BLACK);
+                    squares[row, 11 - column].Occupant = new Pawn(PlayerColor.BLACK);
+                    squares[11 - row, column].Occupant = new Pawn(PlayerColor.WHITE);
+                    squares[11 - row, 11 - column].Occupant = new Pawn(PlayerColor.WHITE);
+                }
+            }
+        }
         public Square GetSquare(int row, int col)
         {
             if (row < 0 || col < 0 || row > Size - 1 || col > Size - 1)
