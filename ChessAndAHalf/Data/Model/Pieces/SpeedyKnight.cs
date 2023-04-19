116d9a89396e4effcb7db8ee3aae0d91ed6bd1d6
DianaBuruga <100471189+DianaBuruga@users.noreply.github.com>
2023-04-19 12:56:40 +0300

    Add Knight, Pawn, Queen, Rook, SpeedyKnight classes

---
+++ ChessAndAHalf/Data/Model/Pieces/SpeedyKnight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessAndAHalf.Data.Model.Pieces
+{
+    internal class SpeedyKnight : Piece
+    {
+        public SpeedyKnight(PlayerColor color) : base(color)
+        {
+
+        }
+        public override string GetImagePath()
+        {
+            return Color == PlayerColor.WHITE ? "../../Images/Pieces/whiteSpeedyKnight.png" : "../../Images/Pieces/blackSpeedyKnight.png";
+        }
+
+        public override List<Position> GetLegalMoves(Board board, Square currentSquare)
+        {
+            List<Position> legalMoves = new List<Position>();
+            /*
+            mutari legale
+            */
+            return legalMoves;
+        }
+    }
+}
